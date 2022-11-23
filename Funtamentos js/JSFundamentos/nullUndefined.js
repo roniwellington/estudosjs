8e00537536312e943fd119eec74417532f24ac2c
Roni Wellington <roniwellington@gmail.com>
2022-11-23 16:07:00 -0300

    teste commit

---
+++ Funtamentos js/JSFundamentos/nullUndefined.js
@@ -0,0 +1,11 @@
+let input = null;
+let input2
+
+// if(input === null) {
+//     console.log('não hã informação')
+// } else {
+//     console.log(input)
+// }
+
+console.log(input);
+console.log(input2)
