9090b81dff1a5e21b298cd8b4553f6fc5da4aa2f
Roni Wellington <roniwellington@gmail.com>
2022-11-23 18:06:27 -0300

    cont. estudo

---
+++ Fundamentosjs/JSFundamentos/moduleDois/script.js
@@ -0,0 +1,33 @@
+// let altura = 5;
+// let comprimento = 7;
+
+// let area = altura * comprimento;
+
+// console.log(area)
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+//  if(forma === 'retângulo') {
+//     area = altura * comprimento;
+//  } else {
+//     area = (altura * comprimento) / 2
+//  }
+
+// console.log(area)
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === 'quadrado'){
+    area = altura * comprimento;
+}else {
+    area = (altura * comprimento) / 2;
+}
+
+// const area = altura * comprimento;
+console.log(area)
---
+++ Fundamentosjs/JSFundamentos/moduleDois/truthy-falsy.js
@@ -0,0 +1,24 @@
+//boolean
+
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// console.log(0 == false) 
+// console.log("" == false) 
+// console.log(1 == true) 
+
+//null ==> vazio ou nada
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+// console.log(minhaVar)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
