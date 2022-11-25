3148b4b94a57eebe0e2a59360a1747252116601e
Roni Wellington <roniwellington@gmail.com>
2022-11-25 19:41:32 -0300

    mudei para outra pasta esses códigos

---
+++ Fundamentosjs/desafioSeteDias/diaDois.js
@@ -0,0 +1,6 @@
+const nome = ("Qual o seu nome?");
+const idade = ("Quantos anos você tem?");
+const linguagem = ("Qual linguagem de programação você está estudando?")
+const msg = `Olá ${nome}, você tem${idade} anos e já está aprendendo ${linguagem}`
+
+console.log(msg)
