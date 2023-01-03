7331ff5dcccb8e8a2867874835b55979ce4af4c8
Roni Wellington <roniwellington@gmail.com>
2023-01-03 17:21:02 -0300

    ESTUDO DE LOGICA

---
+++ youtube/Serliv/script.js
@@ -0,0 +1,21 @@
+
+// let nome = prompt("Qual seu nome?");
+
+// let endereco = prompt("");
+
+
+// alert("Bem vindo " + nome + " ao endereço " + endereco);
+
+//pedir para  o usuario digitar un numero
+ let n1 = prompt("Digite um número");
+ n1 = parseFloat(n1);
+
+//pedir para o usuario digitar outro numero
+
+var n2 =  prompt ("Digite mais um número");
+
+n2 = parseFloat(n2);
+
+//mostra a soma
+
+alert(n1 + n2)
