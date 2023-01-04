cbc4733996d2d6636a74537ab6d1b2bee1df8545
Roni Wellington <roniwellington@gmail.com>
2023-01-04 09:56:04 -0300

    logica de programação

---
+++ youtube/Serliv-logica/aula5.js
@@ -0,0 +1,4 @@
+// Operadoreslógicos
+
+// pedir para o usuario digitar dois números e mostrar uma mensagem informando se foi aprovado ou não.A nota de corte é 5.
+// Contudo, se qualquer uma das notas for zero, reprova automaticamemte
