8c444f65cd96b311fdd88367fe80167ff48accfb
Roni Wellington <roniwellington@gmail.com>
2023-01-06 10:43:34 -0300

    TESTE

---
+++ livros/logica/capitulo1.js
@@ -0,0 +1,53 @@
+// alert("podemos fazer mais com JavaScript");
+
+// document.write("Minha Idade é: ");
+// document.write(25 + "<br>");
+
+// document.write("Eu nasci em: ");
+// document.write(2012 - 25 + "<br>");
+
+// document.write("A soma das nossas idades é: ");
+// document.write(25 + 32 + 26 + "<br>")
+
+// document.write("A media das nossas idades é: ");
+// document.write((25 + 32 + 26)/3 + "<br>")
+
+// let idadeIrmãos = 31 - 29;
+
+// document.write("Nossa diferença de idade é de " + idadeIrmãos + " anos <br>")
+
+// document.write(2022 / 28)
+
+
+// let ano = 2023;
+// document.write("Eu nasci em :  " + (ano - 29) + "<br>" );
+// document.write("Adriano nasceu em : " + (ano - 37) + "<br>");
+// document.write("Paulo nasceu em : " + (ano - 43 ) + "<br>");
+
+// let eu = 29;
+// let adriano = 37;
+// let Paulo = 32;
+
+// let total = eu + adriano + Paulo;
+// document.write("A soma das idades é : " + total + "<br>") ;
+
+// let media = total/3;
+// document.write("A média das idades é  " + Math.round(media))
+
+document.write("<h3>álcool ou Gasolina?</h3>")
+
+let totalPercorrido = 480;
+let tamanhoDoTanque = 40;
+let consumoDeGasolina = totalPercorrido/tamanhoDoTanque
+document.write(" O  consumo de gasolina é : " + consumoDeGasolina + " km por litro<br>")
+
+let caminhoComAlcool = 300;
+consumoDeAlcool = caminhoComAlcool/tamanhoDoTanque;
+document.write("O  consumo de alcool é : " + consumoDeAlcool + " km por litro")
+
+document.write("<h3>Preço por KM rodado</h3>")
+
+let
+
+
+
