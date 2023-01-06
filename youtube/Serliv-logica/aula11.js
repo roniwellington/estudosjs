98b2ee2240fa6cb8aedf655dfc0a927b923b6790
Roni Wellington <roniwellington@gmail.com>
2023-01-06 17:53:36 -0300

    logica

--- youtube/Serliv-logica/aula11.js
+++ youtube/Serliv-logica/aula11.js
@@ -1,5 +1,5 @@
 
-let produtos = new Array('caneta',  'lapis', 'tesoura', 'borracha', 'caderno', 'livro');
+let produtos = new Array('caneta', 'lapis', 'tesoura', 'borracha', 'caderno', 'livro');
 
 produtos.push('compasso', 'cartolina')
 
@@ -9,8 +9,19 @@ let output = document.querySelector('#output');
 let msg = '';
 
 for (let i = 0; i < produtos.length; i++) {
-    msg += 'produto ' + (i + 1)  + ': ' +  produtos[i] + ' <br>';
-    
+    msg += 'produto ' + (i + 1) + ': ' + produtos[i] + ' <br>';
+
 }
 
-output.innerHTML = msg ;
+output.innerHTML = msg;
+
+document.write('<h1>Dias da semana</h1>')
+
+
+let dias = new Array('domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado');
+
+let hoje = new Date();
+
+let diaSemana = hoje.getDay(); //retorna o dia da semana
+
+alert('Bem vindo, hoje é ' + diaSemana)
---
+++ youtube/Serliv-logica/aula12.js
@@ -0,0 +1,4 @@
+//mostrar na tela a soma dos números pares da array abaixo
+
+let arr = new Array(25, 30, 45, 28, 0, 12, 78, 64);
+
