1d334e5a26d6eac3dd8fa5972e5bc0f85fce2514
Roni Wellington <roniwellington@gmail.com>
2023-01-09 16:47:13 -0300

    estudo de lógica

--- youtube/Serliv-logica/aula12.js
+++ youtube/Serliv-logica/aula12.js
@@ -1,4 +1,21 @@
 //mostrar na tela a soma dos números pares da array abaixo
 
-let arr = new Array(25, 30, 45, 28, 0, 12, 78, 64);
+let numeros = new Array(1, 2, 3, 5, 2);
 
+//mostrar na tela  a soma dos números pares array acima
+
+somaOsPares(numeros)
+
+function somaOsPares(arr) {
+
+    let soma = 0;
+    for (let i = 0; i < arr.length; i++) {
+        console.log(i);
+        console.log(arr[i]);
+
+        if (!isNaN(arr[i]) && arr[i] % 2 === 0) {
+            soma += arr[i];
+        }
+    }
+    alert('A soma dos numeros pares é ' + soma);
+}
