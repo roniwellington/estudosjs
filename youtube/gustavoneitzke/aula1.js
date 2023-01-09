0e7abfeac649dbca0ddfdb2e22763bdab0937060
Roni Wellington <roniwellington@gmail.com>
2023-01-09 20:48:57 -0300

    estudo

---
+++ youtube/gustavoneitzke/aula1.js
@@ -0,0 +1,23 @@
+function go (){
+    let valueInput = document.getElementById('num').value;
+    alert(valueInput)
+}
+
+function valor (){
+    var a = document.getElementById('num-a').value;
+    var b = document.getElementById('num-b').value;
+    var c = b;
+    var b = a;
+    var a = c;
+    console.log('Variável A: ' + a);
+    console.log('Variável B: ' + b);
+}
+
+function somamedia(){
+    var idade1 = parseInt(document.querySelector('#crianca1').value);
+    var idade2 = parseInt(document.querySelector('#crianca2').value);
+    var soma = (idade1 + idade2);
+    var media = soma/2
+    console.log('Soma: ' + soma)
+    console.log('A media é ' + media)
+}
