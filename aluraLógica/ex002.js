cc337562ddd0883ba70d2154de748add16f3f25f
Roni Wellington <roniwellington@gmail.com>
2023-01-25 23:17:03 -0300

    estudo de lógica

---
+++ aluraLógica/ex002.js
@@ -0,0 +1,34 @@
+// var idadeLinda = 12;
+// var idadeGuaraciara = 15;
+// var idadePerola = 13;
+// var media = (idadeLinda + idadeGuaraciara + idadePerola) / 3;
+// console.log(`A idade de Linda é ${idadeLinda}, da Guaraciara é ${idadeGuaraciara} e da Perola é ${idadePerola}`)
+// console.log(`A media da idade das delas é ${Math.round(media)}`)
+
+
+
+// function exibeAlerta(mensagem){
+//     console.log("***" + mensagem + "***");
+// }
+
+// var idade1 = 10;
+// var idade2 = 20;
+// var idade3 = 30;
+
+// var totalIdades = idade1 + idade2 + idade3;
+// var mediaIdade = totalIdades/3
+
+// exibeAlerta("Total de idades é " + totalIdades)
+// exibeAlerta("Á media das idades é " + mediaIdade)
+
+
+function pulaLinha(){
+    console.log('**********************************')
+}
+
+pulaLinha()
+console.log('BEM-VINDO AO MEU PROGRAMA')
+pulaLinha()
+console.log('ELE REALMENTE FUNCIONA')
+pulaLinha()
+console.log('EU SEI FUNÇÃO PARA FAZER ISSO')
